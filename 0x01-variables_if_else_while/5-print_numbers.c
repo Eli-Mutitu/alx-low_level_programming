972082537560bbb6509d0d3061df3ead62dee84c
“Eli <eliforestdev@gmail.com>
2023-08-14 15:02:34 -0700

    5-print_numbers.c commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,13 @@
 
 /**
  * main - prints all single digit numbers of base
- * 10 starting from 0, followed by a new line
+ * 10 starting from 0, followed by a new line.
+ *
  * return: always 0 (Success)
  */
+
 int main(void)
+
 {
 	int n;
 
