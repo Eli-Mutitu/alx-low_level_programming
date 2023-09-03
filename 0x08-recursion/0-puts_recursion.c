732cf8711beff9aabbde63daab2001d4c6b12037
“Eli <eliforestdev@gmail.com>
2023-09-03 12:21:27 -0700

    0-puts_recursion.c commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,24 +1,17 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _puts_recursion - Prints a string followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 
-	putchar(*s);
-	_puts_recursion(s + 1);
-}
-
-int main(void)
-{
-	char str[] = "Hello, World!";
-	_puts_recursion(str);
-	return (0);
+	else
+		_putchar('\n');
 }
