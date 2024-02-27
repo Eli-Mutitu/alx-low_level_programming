29f9b87132ac7dbf9403dae1f72236c118870b54
Eli-Mutitu <eliforestdev@gmail.com>
2024-02-27 03:13:54 -0800

    revision commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,66 +1,84 @@
-#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdint.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <elf.h>
+
+#define BUF_SIZE 64
+
+void print_error(const char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(98);
+}
 
 void print_elf_header(const char *filename) {
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: Unable to open file '%s'\n", filename);
-        exit(98);
-    }
-
+    int fd;
     Elf64_Ehdr header;
-    ssize_t bytes_read = read(fd, &header, sizeof(Elf64_Ehdr));
-    if (bytes_read != sizeof(Elf64_Ehdr)) {
-        fprintf(stderr, "Error: Unable to read ELF header from file '%s'\n", filename);
-        close(fd);
-        exit(98);
+    ssize_t bytes_read;
+
+    // Open the ELF file
+    fd = open(filename, O_RDONLY);
+    if (fd == -1) {
+        print_error("Error opening file");
     }
 
-    // Check ELF magic
-    if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-        header.e_ident[EI_MAG1] != ELFMAG1 ||
-        header.e_ident[EI_MAG2] != ELFMAG2 ||
-        header.e_ident[EI_MAG3] != ELFMAG3) {
-        fprintf(stderr, "Error: File '%s' is not an ELF file\n", filename);
-        close(fd);
-        exit(98);
+    // Read ELF header
+    bytes_read = read(fd, &header, sizeof(header));
+    if (bytes_read == -1 || bytes_read != sizeof(header)) {
+        print_error("Error reading ELF header");
+    }
+
+    // Check ELF magic number
+    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
+        print_error("Not an ELF file");
     }
 
     // Print ELF header information
+    printf("ELF Header:\n");
     printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++) {
-        printf(" %02x", header.e_ident[i]);
+    for (int i = 0; i < SELFMAG; i++) {
+        printf("%02x ", header.e_ident[i]);
     }
     printf("\n");
 
-    printf("  Class:                             %s\n", header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-    printf("  Data:                              %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Class:                             %s\n", (header.e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
     printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
-    printf("  OS/ABI:                            %d\n", header.e_ident[EI_OSABI]);
+    printf("  OS/ABI:                            ");
+    switch (header.e_ident[EI_OSABI]) {
+        case ELFOSABI_SYSV: printf("UNIX - System V\n"); break;
+        case ELFOSABI_HPUX: printf("UNIX - HP-UX\n"); break;
+        case ELFOSABI_NETBSD: printf("UNIX - NetBSD\n"); break;
+        case ELFOSABI_LINUX: printf("UNIX - Linux\n"); break;
+        case ELFOSABI_SOLARIS: printf("UNIX - Solaris\n"); break;
+        case ELFOSABI_IRIX: printf("UNIX - IRIX\n"); break;
+        case ELFOSABI_FREEBSD: printf("UNIX - FreeBSD\n"); break;
+        case ELFOSABI_TRU64: printf("UNIX - TRU64\n"); break;
+        case ELFOSABI_ARM: printf("ARM\n"); break;
+        case ELFOSABI_STANDALONE: printf("Standalone (embedded) application\n"); break;
+        default: printf("<unknown: %d>\n", header.e_ident[EI_OSABI]); break;
+    }
     printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-
     printf("  Type:                              ");
     switch (header.e_type) {
-        case ET_NONE: printf("NONE (Unknown type)\n"); break;
+        case ET_NONE: printf("NONE (No file type)\n"); break;
         case ET_REL: printf("REL (Relocatable file)\n"); break;
         case ET_EXEC: printf("EXEC (Executable file)\n"); break;
         case ET_DYN: printf("DYN (Shared object file)\n"); break;
         case ET_CORE: printf("CORE (Core file)\n"); break;
-        default: printf("<unknown>\n"); break;
+        default: printf("<unknown: %d>\n", header.e_type); break;
     }
+    printf("  Entry point address:               0x%lx\n", (unsigned long) header.e_entry);
 
-    printf("  Entry point address:               %#lx\n", (unsigned long)header.e_entry);
-
+    // Close the file descriptor
     close(fd);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return 98;
+        print_error("Usage: elf_header elf_filename");
     }
 
     print_elf_header(argv[1]);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,104 +1,70 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+#define BUF_SIZE 1024
 
-/**
- * creates_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
-char *creates_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
+void print_error(const char *message, int code) {
+    dprintf(STDERR_FILENO, "%s\n", message);
+    exit(code);
 }
 
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
+int main(int argc, char *argv[]) {
+    int fd_from, fd_to;
+    ssize_t bytes_read, bytes_written;
+    char buffer[BUF_SIZE];
 
-	c = close(fd);
+    // Check for correct number of arguments
+    if (argc != 3) {
+        print_error("Usage: cp file_from file_to", 97);
+    }
 
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
+    // Open source file
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
 
-/**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
- */
-int main(int argc, char *argv[])
-{
-	int from, to, r, w;
-	char *buffer;
+    // Open destination file, truncate if it exists
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (fd_to == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close(fd_from);
+        exit(99);
+    }
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+    // Copy file content
+    while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0) {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written != bytes_read) {
+            dprintf(STDERR_FILENO, "Error: Write to %s failed\n", argv[2]);
+            close(fd_from);
+            close(fd_to);
+            exit(99);
+        }
+    }
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (bytes_read == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(fd_from);
+        close(fd_to);
+        exit(98);
+    }
 
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
+    // Close file descriptors
+    if (close(fd_from) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+    if (close(fd_to) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
-
-	return (0);
+    return 0;
 }
 
